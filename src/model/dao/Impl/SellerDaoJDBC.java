cd89cf82c70e478fa67b8c623e1b5a3a88154e14
Launyx <lunyx.castelhano@gmail.com>
2024-06-07 15:50:32 -0300

    findById implementation

--- src/model/dao/Impl/SellerDaoJDBC.java
+++ src/model/dao/Impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.Impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller obj) {
@@ -24,7 +37,52 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            // Implementação da consulta (statement) no bando de dados
+            st = conn.prepareStatement(
+                    "SELECT seller.*, department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ? ");
+
+            // Definindo valor a subtituir '?' no statement
+            st.setInt(1, id);
+
+            // Execução da consulta (statement) no banco e armazenamento da tabela resultante na variável rs (ResultSet)
+            rs = st.executeQuery();
+
+            /* Quando é executada uma consulta (statement) e o resultado é colocado em um ResultSet, este rs aponta
+             para a posição 0, que não contém objeto. Por isso é utilizado um if com rs.next() para testar se houve
+             algum resultado como retorno
+             */
+            if (rs.next()){
+                // Instanciação do departamento que todo Seller tem
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+
+                // Instaciação do Seller
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("iD"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dep);
+                return obj;
+            }
+            // Retorna nulo caso não houver retorno de objeto (não há seller com a consulta determinada)
+            return null;
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
